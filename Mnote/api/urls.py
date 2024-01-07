63f57a6dbd662561ba6cdd6ee0f1743e45b4293f
Subhash Khambampati <153987285+SubhashKhambampati@users.noreply.github.com>
2024-01-07 22:49:49 +0530

    Delete Mnote directory

--- Mnote/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin 
-from django.urls import path  , include
-from api import views
-
-urlpatterns = [
-    
-    path('' , views.getRoutes , name='routes'),
-    path('note' , views.getNote , name='Note'),
-]
